0c4a53ca02633deb068a29c8d08de46671e7413a
Jacob Malcom <jmalcom@defenders.org>
2016-05-03 15:01:40 -0400

    Completed the coord conversion.

--- data_prep/R/sec7_prep_v3/convert_coords.R
+++ data_prep/R/sec7_prep_v3/convert_coords.R
@@ -58,7 +58,7 @@ convert_utm <- function(E, N, zone, datum) {
 
 ##################################
 # Now for all of the NAD27 samples
-nad27 <- full[full$datum == "NAD27" & !is.na(full$datum), ]
+nad27 <- full[full$datum == "NAD27" & !is.na(full$datum) & full$datum != "", ]
 nad27_dd <- nad27[!is.na(nad27$lat_dec_deg), ]
 nad27_dms <- nad27[!is.na(nad27$lat_deg), ]
 nad27_utm <- nad27[!is.na(nad27$UTM_E), ]
@@ -66,18 +66,26 @@ dim(nad27_dd)
 dim(nad27_dms)
 dim(nad27_utm)
 
-nad27_dd$nad83dd <- convert_dd(nad27_dd[,27], nad27_dd[,26], "NAD27")
+nad27_dd_nad83dd <- convert_dd(nad27_dd[,27], nad27_dd[,26], "NAD27")
+nad27_dd$nad83_lon <- nad27_dd_nad83dd[,1]
+nad27_dd$nad83_lat <- nad27_dd_nad83dd[,2]
+
 nad27dms_dec_lat <- dms2dd(nad27_dms[, 28], nad27_dms[, 29], nad27_dms[, 30])
 nad27dms_dec_lon <- dms2dd(nad27_dms[, 31], nad27_dms[, 32], nad27_dms[, 33], neg=TRUE)
-nad27_dms$nad83dd <- convert_dd(nad27dms_dec_lon, nad27dms_dec_lat, "NAD27")
-nad27_utm$nad83dd <- convert_utm(nad27_utm[, 34], nad27_utm[, 35],
+nad27_dms_nad83dd <- convert_dd(nad27dms_dec_lon, nad27dms_dec_lat, "NAD27")
+nad27_dms$nad83_lon <- nad27_dms_nad83dd[,1]
+nad27_dms$nad83_lat <- nad27_dms_nad83dd[,2]
+
+nad27_utm_nad83dd <- convert_utm(nad27_utm[, 34], nad27_utm[, 35],
                                  nad27_utm[, 36], "NAD27")
+nad27_utm$nad83_lon <- nad27_utm_nad83dd[,1]
+nad27_utm$nad83_lat <- nad27_utm_nad83dd[,2]
 
 nad27_nad83 <- rbind(nad27_dd, nad27_dms, nad27_utm)
 
 ##################################
 # Now for all of the NAD83 samples
-nad83 <- full[full$datum == "NAD83" & !is.na(full$datum), ]
+nad83 <- full[full$datum == "NAD83" & !is.na(full$datum) & full$datum != "", ]
 nad83_dd <- nad83[!is.na(nad83$lat_dec_deg), ]
 nad83_dms <- nad83[!is.na(nad83$lat_deg), ]
 nad83_utm <- nad83[!is.na(nad83$UTM_E), ]
@@ -86,10 +94,27 @@ dim(nad83_dd)
 dim(nad83_dms)
 dim(nad83_utm)
 
+nad83_dd_nad83dd <- convert_dd(nad83_dd[,27], nad83_dd[,26], "NAD83")
+nad83_dd$nad83_lon <- nad83_dd_nad83dd[,1]
+nad83_dd$nad83_lat <- nad83_dd_nad83dd[,2]
+
+nad83dms_dec_lat <- dms2dd(nad83_dms[, 28], nad83_dms[, 29], nad83_dms[, 30])
+nad83dms_dec_lon <- dms2dd(nad83_dms[, 31], nad83_dms[, 32], nad83_dms[, 33], neg=TRUE)
+nad83_dms_nad83dd <- convert_dd(nad83dms_dec_lon, nad83dms_dec_lat, "NAD83")
+nad83_dms$nad83_lon <- nad83_dms_nad83dd[,1]
+nad83_dms$nad83_lat <- nad83_dms_nad83dd[,2]
+
+nad83_utm_nad83dd <- convert_utm(nad83_utm[, 34], nad83_utm[, 35],
+                                 nad83_utm[, 36], "NAD83")
+nad83_utm$nad83_lon <- nad83_utm_nad83dd[,1]
+nad83_utm$nad83_lat <- nad83_utm_nad83dd[,2]
+
+nad83_nad83 <- rbind(nad83_dd, nad83_dms, nad83_utm)
+
 
 ##################################
 # Now for all of the WGS84 samples
-wgs84 <- full[full$datum == "WGS84" & !is.na(full$datum), ]
+wgs84 <- full[full$datum == "WGS84" & !is.na(full$datum) & full$datum != "", ]
 wgs84_dd <- wgs84[!is.na(wgs84$lat_dec_deg), ]
 wgs84_dms <- wgs84[!is.na(wgs84$lat_deg), ]
 wgs84_utm <- wgs84[!is.na(wgs84$UTM_E), ]
@@ -97,4 +122,46 @@ dim(wgs84)
 dim(wgs84_dd)
 dim(wgs84_dms)
 dim(wgs84_utm)
+
+wgs84_dd_nad83dd <- convert_dd(wgs84_dd[,27], wgs84_dd[,26], "WGS84")
+wgs84_dd$nad83_lon <- wgs84_dd_nad83dd[,1]
+wgs84_dd$nad83_lat <- wgs84_dd_nad83dd[,2]
+
+wgs84dms_dec_lat <- dms2dd(wgs84_dms[, 28], wgs84_dms[, 29], wgs84_dms[, 30])
+wgs84dms_dec_lon <- dms2dd(wgs84_dms[, 31], wgs84_dms[, 32], wgs84_dms[, 33], neg=TRUE)
+wgs84_dms_nad83dd <- convert_dd(wgs84dms_dec_lon, wgs84dms_dec_lat, "WGS84")
+wgs84_dms$nad83_lon <- wgs84_dms_nad83dd[,1]
+wgs84_dms$nad83_lat <- wgs84_dms_nad83dd[,2]
+
+wgs84_utm_nad83dd <- convert_utm(wgs84_utm[, 34], wgs84_utm[, 35],
+                                 wgs84_utm[, 36], "WGS84")
+wgs84_utm$nad83_lon <- wgs84_utm_nad83dd[,1]
+wgs84_utm$nad83_lat <- wgs84_utm_nad83dd[,2]
+
+wgs84_nad83 <- rbind(wgs84_dd, wgs84_dms, wgs84_utm)
+
+##################################
+# And combined the data
+with_coord <- rbind(nad27_nad83, nad83_nad83, wgs84_nad83)
+
+no_coord <- full[is.na(full$datum) | full$datum == "", ]
+dim(no_coord)
+
+no_coord$nad83_lon <- rep(NA, length(no_coord[,1]))
+no_coord$nad83_lat <- rep(NA, length(no_coord[,1]))
+dim(no_coord)
+
+names(with_coord) == names(no_coord)
+
+semi <- rbind(with_coord, no_coord)
+names(semi)
+
+fin <- semi[, c(1:22, 37:51)]
+fin$datum <- rep("NAD83", length(fin$nad83_lon))
+names(fin)[36:37] <- c("long", "lat")
+names(fin)
+
+full <- fin
+save(full, file="FWS_S7_clean_03May2016_0-3.RData")
+
 
